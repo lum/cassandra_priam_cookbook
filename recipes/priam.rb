0bd88bd74e7174ae30d338eaf4d51b01189c51e7
Alex Trull <alextrull@getchef.com>
2014-05-29 15:39:44 +0100

    set commands for tomcat7 sudo entry.

--- recipes/priam.rb
+++ recipes/priam.rb
@@ -21,7 +21,7 @@
 sudo node[:tomcat][:user] do
   user node[:tomcat][:user]
   runas "root"
-#  commands ['/etc/init.d/cassandra start', '/etc/init.d/cassandra stop'] 
+  commands ['/etc/init.d/cassandra start', '/etc/init.d/cassandra stop'] 
   nopasswd true
   defaults ['!requiretty','env_reset']
 end
